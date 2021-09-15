181007b168876ebc16c4f4bbbe683e4042842171
jinny <jinnyszy@gmail.com>
2021-09-16 04:30:33 +0800

    git commit

--- src/App.js
+++ src/App.js
@@ -1,105 +1,264 @@
-import { useEffect, useState, useRef } from "react";
-import Navigation from "./Components/Navigation";
-import Chart from "./Chart";
-import "./App.css";
+import React, {Component} from 'react';
+import { Line, Bar, Pie, PolarArea, Radar } from 'react-chartjs-2';
+import './App.css';
+import {readString} from 'react-papaparse';
+import siteListCSV from './road.csv';
 
-const initialData = {
-  labels: [],
-  datasets: [
-    {
-      label: "INDEX",
-      fill: false,
-      lineTension: 0.1,
-      backgroundColor: "rgba(153, 102, 255, 0.2)",
-      borderColor: "rgba(153, 102, 255, 1)",
-      borderCapStyle: "butt",
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: "miter",
-      pointBorderColor: "brown",
-      pointBackgroundColor: "rgba(153, 102, 255, 0.2)",
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "violet",
-      pointHoverBorderColor: "green",
-      pointHoverBorderWidth: 5,
-      pointRadius: 1,
-      pointHitRadius: 1,
-      data: []
+const year = []; // year label
+const arr = []; //index
+var pop = [];  //population
+var roadDeath = []; //roadDeaths
+var roadCrash = []; //road crashes
+var reg = []; //registeredVehicles
+var serInjury = []; //seriousInjury
+var slInjury = [] //slight injury
+
+const search = ',';
+const replaceWith = '';
+
+const parse = {
+  complete: (results, file)=>{
+    console.log('PArsing complete: ', results, file);
+    for(let i=0;i<21;i++){
+      year[i]=results.data[i].Year;
+      pop[i] = results.data[i].Population.split(search).join(replaceWith);
+      arr[i]=results['data'][i]['Index per 100,000 Population'];
+      roadDeath[i]=parseInt(results['data'][i]['Road Deaths'].replace(/,/g,''));
+      roadCrash[i]=parseInt(results['data'][i]['Road Crashes'].replace(/,/g,''));
+      reg[i]=parseInt(results['data'][i]['Registered Vehicles'].replace(/,/g,''));
+      serInjury[i]=parseInt(results['data'][i]['Serious Injury'].replace(/,/g,''));
+      slInjury[i]=parseInt(results['data'][i]['Slight Injury'].replace(/,/g,''));
+      console.log(year[i]);
+      console.log(arr[i]);
+      console.log(pop[i]);
+      console.log(roadDeath[i]);
+      console.log(roadCrash[i]);
+      console.log(reg[i]);
+      console.log(serInjury[i]);
+      console.log(slInjury[i]);
     }
-  ]
+
+  },
+  header: true,
+  download: true,
+  error: (error, file)=>{
+    console.log('Error: ', error, file);
+  },
 };
+readString(siteListCSV, parse);
 
-export default function App() {
-  const [isValidFile, setIsValidFile] = useState(null);
-  const [file, setFile] = useState(null);
-  const [headers, setHeaders] = useState(null);
-  const [selectedHeader, setSelectedHeader] = useState("none");
-  const [active, setActive] = useState(null);
-  const defaultData = useRef(initialData);
+function App() {
 
-  useEffect(() => {
-    const data = {
-      labels: file?.map((el) => el.data[0]),
-      datasets: [
-        {
-          ...defaultData.current.datasets[0],
-          data: file?.map((el) => el.data[selectedHeader])
-        }
-      ]
-    };
-    setActive(data);
-  }, [selectedHeader, file, defaultData.current.datasets]);
+  const data = {
+    labels: year,
+    datasets: [
+      {
+        label: "Index per 100,000 population",
+        data: arr, //change to display the wanted data (population, registered vehicle, index etc)
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: "rgba(153, 102, 255, 0.2)",
+        borderColor: "rgba(153, 102, 255, 1)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "brown",
+        pointBackgroundColor: "rgba(153, 102, 255, 0.2)",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "violet",
+        pointHoverBorderColor: "green",
+        pointHoverBorderWidth: 5,
+        pointRadius: 1,
+        pointHitRadius: 1
+      }
+    ]
+  };
 
-  function handleUpload(file) {
-    const { data: heads } = file?.shift() || { headers: null };
+  const data1 = {
+    labels: year,
+    datasets: [
+      {
+        label: "Population",
+        data: pop, //change to display the wanted data (population, registered vehicle, index etc)
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(74, 255, 196, 0.5)",
+        borderColor: "rgba(74, 255, 196, 1)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "brown",
+        pointBackgroundColor: "rgba(74, 255, 196, 1)",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "violet",
+        pointHoverBorderColor: "green",
+        pointHoverBorderWidth: 5,
+        pointRadius: 1,
+        pointHitRadius: 1
+      }
+    ]
+  };
 
-    setFile(file);
-    setHeaders(heads);
-  }
+  const data2 = {
+    labels: year,
+    datasets: [
+      {
+        label: "Road Crashes",
+        data: roadCrash, //change to display the wanted data (population, registered vehicle, index etc)
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: "rgba(16, 220, 255, 0.5)",
+        borderColor: "rgba(16, 220, 255, 1)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "brown",
+        pointBackgroundColor: "rgba(16, 220, 255, 0.5)",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "violet",
+        pointHoverBorderColor: "green",
+        pointHoverBorderWidth: 5,
+        pointRadius: 1,
+        pointHitRadius: 1
+      }
+    ]
+  };
 
-  function handleSelectHeader(e) {
-    console.log(e.target.value);
-    setSelectedHeader(e.target.value);
-  }
+  const data3 = {
+    labels: year,
+    datasets: [
+      {
+        label: "Road Deaths",
+        data: roadDeath, //change to display the wanted data (population, registered vehicle, index etc)
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: "rgba(255, 159, 64, 0.2)",
+        borderColor: "rgba(255, 159, 64, 1)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "brown",
+        pointBackgroundColor: "rgba(255, 159, 64, 0.2)",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "violet",
+        pointHoverBorderColor: "green",
+        pointHoverBorderWidth: 5,
+        pointRadius: 1,
+        pointHitRadius: 1
+      }
+    ]
+  };
+
+  const data4 = {
+    labels: year,
+    datasets: [
+      {
+        label: "Registered Vehicles",
+        data: reg, //change to display the wanted data (population, registered vehicle, index etc)
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: "rgba(235, 51, 149, 0.2)",
+        borderColor: "rgba(235, 51, 149, 0.6)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "brown",
+        pointBackgroundColor: "rgba(235, 51, 149, 0.5)",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "violet",
+        pointHoverBorderColor: "green",
+        pointHoverBorderWidth: 5,
+        pointRadius: 1,
+        pointHitRadius: 1
+      }
+    ]
+  };
+
+  const data5 = {
+    labels: year,
+    datasets: [
+      {
+        label: "Serious Injury",
+        data: serInjury, //change to display the wanted data (population, registered vehicle, index etc)
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: "rgba(16, 220, 39, 0.5)",
+        borderColor: "rgba(16, 220, 39, 1)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "brown",
+        pointBackgroundColor: "rgba(16, 220, 39, 0.5)",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "violet",
+        pointHoverBorderColor: "green",
+        pointHoverBorderWidth: 5,
+        pointRadius: 1,
+        pointHitRadius: 1
+      },
+      {
+        label: "Slight Injury",
+        data: slInjury, //change to display the wanted data (population, registered vehicle, index etc)
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: "rgba(174, 255, 74, 0.5)",
+        borderColor: "rgba(174, 255, 74, 1)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "brown",
+        pointBackgroundColor: "rgba(174, 255, 74, 0.5)",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "violet",
+        pointHoverBorderColor: "green",
+        pointHoverBorderWidth: 5,
+        pointRadius: 1,
+        pointHitRadius: 1
+      }
+    ]
+  };
 
   return (
-    <div className="App">
-      <Navigation
-        uploadFile={(e) => handleUpload(e)}
-        isValid={(e) => setIsValidFile(e)}
-      >
-        <select
-          value={selectedHeader}
-          onChange={handleSelectHeader}
-          className="form-select btn btn-outline-success w-50"
-          aria-label="Default select example"
-        >
-          <option value="none">select a value</option>
-          {headers?.map((val, _i) => {
-            return (
-              <option
-                key={val}
-                value={_i}
-                style={{
-                  display: `${val === "Date" ? "none" : "block"}`
-                }}
-              >
-                {val}
-              </option>
-            );
-          })}
-        </select>
-      </Navigation>
+<>
+<div
+  style={{
+    alignItems: "center",
+    marginLeft: "auto",
+    marginRight: "auto",
+    height:600,
+    width:800,
+    flexDirection: "row",
+    justifyContent:"center",
+    flexWrap: "wrap"
 
-      <h1 className="m-5">Display Chart</h1>
-      {isValidFile === null ? (
-        <h2 className="m-5">Please upload a csv file</h2>
-      ) : !isValidFile ? (
-        <h2 className="m-5">The file must have a csv Extencion</h2>
-      ) : (
-        <Chart data={active} />
-      )}
+  }}
+>
+    <h4>Line Chart - Index per 100,000 Population</h4>
+    <Line data={data} options={{responsive: true, maintainAspectRatio: true,}} />
+    <h4>Bar Chart - Population</h4>
+    <Bar data={data1} />
+    <h4>Pie Chart - Road Deaths</h4>
+    <Pie data={data3} />
+    <h4>Polar Chart - Registered Vehicles</h4>
+    <PolarArea data={data4} />
+    <h4>Radar Chart - Slight Injury and Serious Injuries</h4>
+    <Radar data={data5} />
     </div>
+</>
   );
 }
+export default App
--- src/Chart.js
+++ src/Chart.js
@@ -1,32 +1,6 @@
 import React, { Component } from "react";
-import { Line, Bar } from "react-chartjs-2";
+import { Line, Bar, Scatter, Pie, Bubble, Doughnut } from "react-chartjs-2";
 
-// const data = {
-//   labels: ["January", "February", "March", "April", "May", "June", "July"],
-//   datasets: [
-//     {
-//       label: "My First dataset",
-//       fill: false,
-//       lineTension: 0.1,
-//       backgroundColor: "blue",
-//       borderColor: "red",
-//       borderCapStyle: "butt",
-//       borderDash: [],
-//       borderDashOffset: 0.0,
-//       borderJoinStyle: "miter",
-//       pointBorderColor: "brown",
-//       pointBackgroundColor: "yellow",
-//       pointBorderWidth: 1,
-//       pointHoverRadius: 5,
-//       pointHoverBackgroundColor: "violet",
-//       pointHoverBorderColor: "green",
-//       pointHoverBorderWidth: 5,
-//       pointRadius: 3,
-//       pointHitRadius: 10,
-//       data: [65, 59, 80, 81, 56, 55, 40]
-//     }
-//   ]
-// };
 
 export default class Chart extends Component {
   render() {
@@ -48,6 +22,15 @@ export default class Chart extends Component {
         <Bar ref="chart" data={this.props.data} />
         <h4>Line Chart</h4>
         <Line ref="chart" data={this.props.data} />
+        <h4>Scatter</h4>
+        <Scatter ref="chart" data={this.props.data} />
+        <h4>Pie Chart</h4>
+        <Pie ref="chart" data={this.props.data} />
+        <h4>Bubble Chart</h4>
+        <Bubble ref="chart" data={this.props.data} />
+        <h4>Doughnut</h4>
+        <Doughnut ref="chart" data={this.props.data} />
+
       </div>
     );
   }
